49370da582423a33046eccf3b065f6613aa0de59
mostafa-rayhan <mostafarayhan57@gmail.com>
2023-08-24 16:44:33 +0600

    model update

--- asset_tracking_app/models.py
+++ asset_tracking_app/models.py
@@ -6,10 +6,16 @@ class Company(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 class Employee(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name
 
 class Device(models.Model):
     DEVICE_TYPES = (
@@ -20,6 +26,9 @@ class Device(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=10, choices=DEVICE_TYPES)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 class DeviceLog(models.Model):
     device = models.ForeignKey(Device, on_delete=models.CASCADE)
